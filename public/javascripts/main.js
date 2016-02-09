7b2c41428e55030dc7c7e1acb708070c35c7d21e
keenanzucker <keenan.zucker@students.olin.edu>
2016-02-09 18:53:52 -0500

    front end updating for twotes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
 
+});
+
+
+var $twoteForm = $('#add-twote');
+var $twoteList = $('#twote-list');
+var $templateLi = $('#hidden-template-li');
+
+
+$twoteForm.submit(function(event){
+	event.preventDefault();
+
+	var twote = $twoteForm.find("input[name='name']").val();
+
+	var $newLi = $templateLi.clone();
+	$newLi.removeAttr('id');
+	$newLi.find('.twote').html(twote);
+
+	$twoteList.append($newLi);
+
+	$("#add-twote")[0].reset();
 
 });
